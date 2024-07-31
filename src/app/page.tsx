b5d15a5b841d2bc853a9ae6695ae38fe6870a386
katsisnik <katsisnik@gmail.com>
2024-07-31 15:04:09 +0100

    Added Section.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 'use client'
 
 import Header from "./components/Header";
+import Section from "./components/Section";
 
 export default function Home() {
   return (
     <>
       <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
         <Header />
+        {/* <Section /> */}
       </div>
       
     </>
