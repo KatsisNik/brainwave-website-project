bdfe9562d7ff2d9da41d2c361a90bfe57fcbb9a5
katsisnik <katsisnik@gmail.com>
2024-08-05 13:27:32 +0100

    Completed Hero.tsx and re-usable components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 'use client'
 
 import Header from "./components/Header";
-import Section from "./components/Section";
+import Hero from "./components/Hero";
 
 export default function Home() {
   return (
     <>
       <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
         <Header />
-        {/* <Section /> */}
+        <Hero />
       </div>
       
     </>
