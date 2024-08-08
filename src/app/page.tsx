c1a583813873f3d8d3f7e989c53e0f0c5794bfc1
katsisnik <katsisnik@gmail.com>
2024-08-08 16:39:25 +0100

    Added Services.tsx, updated Heading.tsx for text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Benefits from "./components/Benefits";
 import Collaboration from "./components/Collaboration";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Services from "./components/Services";
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
         <Hero />
         <Benefits />
         <Collaboration />
+        <Services />
       </div>
       
     </>
