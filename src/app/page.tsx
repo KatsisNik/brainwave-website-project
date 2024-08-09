f744c39ff62d586aaa9b87cd666180417b7ed0e4
katsisnik <katsisnik@gmail.com>
2024-08-09 20:08:21 +0100

    Added Roadmap section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Collaboration from "./components/Collaboration";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Pricing from "./components/Pricing";
+import Roadmap from "./components/Roadmap";
 import Services from "./components/Services";
 
 export default function Home() {
@@ -17,6 +18,7 @@ export default function Home() {
         <Collaboration />
         <Services />
         <Pricing />
+        <Roadmap />
       </div>
       
     </>
